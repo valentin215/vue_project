0c7c0d3b47f3505e0b5066fe2ffc587f3edf815f
Valentin Birault <biraultvalentin@gmail.com>
2019-12-27 15:07:09 +0100

    adding styling class when out of stock

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue({
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     onSale: true,
+    inStock: true,
     sizes: ["S", "M", "L", "XL"],
     cart: 0
   },

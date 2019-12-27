7f71d7af78586ba100af0f8291e22c551a0c55f6
Valentin Birault <biraultvalentin@gmail.com>
2019-12-27 14:46:31 +0100

    adding listener

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var app = new Vue({
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
     onSale: true,
-    sizes: ["S", "M", "L", "XL"]
-  }
+    sizes: ["S", "M", "L", "XL"],
+    cart: 0
+  },
+  methods: {
+    removeToCart: function () {
+      this.cart -= 1
+    },
+    addToCart: function () {
+      this.cart += 1
+    },
+  },
 })

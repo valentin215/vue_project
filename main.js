37fa123ad129239abc7ae35c04264a9b7c8bc903
Valentin Birault <biraultvalentin@gmail.com>
2019-12-28 16:46:24 +0100

    adding if statement with computed attributes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery'
     product: 'Socks',
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
@@ -15,6 +16,14 @@ var app = new Vue({
     },
     addToCart: function () {
       this.cart += 1
+    },
+  },
+  computed: {
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + 'are on Sale !'
+      }
+        return this.brand + ' ' + this.product + 'are not on Sale !'
     },
   },
 })

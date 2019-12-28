e015b7d7ea696db04e4e4a3fb2a2403ed75cd074
Valentin Birault <biraultvalentin@gmail.com>
2019-12-28 16:48:06 +0100

    refacto + fixing bug

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    brand: 'Vue Mastery'
+    brand: 'Vue Mastery',
     product: 'Socks',
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
@@ -11,19 +11,19 @@ var app = new Vue({
     cart: 0
   },
   methods: {
-    removeToCart: function () {
+    removeToCart() {
       this.cart -= 1
     },
-    addToCart: function () {
+    addToCart() {
       this.cart += 1
     },
   },
   computed: {
     sale() {
       if (this.onSale) {
-        return this.brand + ' ' + this.product + 'are on Sale !'
+        return this.brand + ' ' + this.product + ' are on Sale !'
       }
-        return this.brand + ' ' + this.product + 'are not on Sale !'
+        return this.brand + ' ' + this.product + ' are not on Sale !'
     },
   },
 })

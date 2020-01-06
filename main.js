548f9d87fcd71e01dcedb6b95fedc522a44c3d77
Valentin Birault <biraultvalentin@gmail.com>
2020-01-06 20:44:41 +0100

    fixing bug

--- main.js
+++ main.js
@@ -111,7 +111,8 @@ var product = Vue.component('product', {
       <li v-for="review in reviews">
         <p> {{ review.name }} </p>
         <p> {{ review.rating }} </p>
-        <p> {{ reviewreview }} </p>
+        <p> {{ review.review }} </p>
+        <p> {{ review.reco }} </p>
       </li>
     </ul>
     </div>
